8025c3c842bacbc340290f514e86df818c540a8d
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 15:26:10 +0700

    матрица тестирования

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328
 echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 sudo apt-get update
 sudo apt-get install mono-complete mono-devel
-curl -v https://oscript.io/downloads/1_3_0/deb?bitness=x64 --output os.deb
+curl -v https://oscript.io/downloads/$1/deb?bitness=$2 --output os.deb
 sudo dpkg -i os.deb
 sudo apt install -f
