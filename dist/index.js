629faf292058a4cfc022d8b74583bccdeed6d4d0
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 19:05:41 +0700

    Отказ от использования скрипта .sh

--- dist/index.js
+++ dist/index.js
@@ -955,6 +955,7 @@ async function run() {
     const osVersion = core.getInput('version');
     var osVersionStr = getVersionString(osVersion);
     console.log('Версия: ' + osVersionStr);
+    console.log('Платформа: ' + patform)
     if (patform == 'win32') {
       console.log('Загрузка');
       await exec.exec('curl -v https://oscript.io/downloads/' + osVersionStr + '/exe?bitness=x64 --output oscript.exe');
@@ -971,7 +972,8 @@ async function run() {
 
     } else if (patform == 'linux') {
 
-      await exec.exec('bash scripts/install.sh ' + osVersionStr + ' x64')
+      await exec.exec(installLinux(osVersionStr, 'x64'));
+      // await exec.exec('bash scripts/install.sh ' + osVersionStr + ' x64')
       await exec.exec('oscript --version')
     
     } else {
@@ -992,6 +994,18 @@ function updatePath() {
   const OLD_PATH = process.env.PATH;
   PATH = OLD_PATH + ";C:\/Program Files\/OneScript\/bin;";
   core.exportVariable('Path', PATH);
+}
+
+function installLinux(version, bitness) {
+ var value = [];
+ value.push('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF');
+ value.push('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list');
+ value.push('sudo apt-get update');
+ value.push('sudo apt-get install mono-complete mono-devel');
+ value.push('curl -v https://oscript.io/downloads/' + version + '/deb?bitness=' + bitness + ' --output os.deb');
+ value.push('sudo dpkg -i os.deb');
+ value.push('sudo apt install -f');
+ return value.join(' && ');
 }
 
 run()
