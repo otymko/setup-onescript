808d523f7343226206bfb23439b87a5873ffd4c8
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 21:50:07 +0700

    +1

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function run() {
       await exec.exec('bash ' + tmpFile.name);
       fs.unlinkSync(tmpFile.name);
       await exec.exec('curl -v https://hub.oscript.io/download/opm/opm.ospx --output opm.ospx');
-      await exec.exec('opm install -f opm.ospx');
+      await exec.exec('sudo opm install -f opm.ospx');
       fs.unlinkSync('opm.ospx');
       await exec.exec('oscript --version');
     
