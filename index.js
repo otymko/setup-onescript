db5fe447c46b7a44474855ec88a837595d7a58fc
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 19:25:23 +0700

    Отказ от sh скрипта часть 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const fs = require('fs');
+const tmp = require('tmp');
 
 const patform = process.platform;
 
@@ -22,13 +23,16 @@ async function run() {
       updatePath();
 
       console.log('Удаление временного файла');
-      await fs.unlinkSync('./oscript.exe');
+      fs.unlinkSync('./oscript.exe');
 
     } else if (patform == 'linux') {
+      
+      var tmpFile = tmp.fileSync();
+      fs.writeFileSync(tmpFile.name, installLinux(osVersionStr, 'x64'));
+      await exec.exec('bash ' + tmpFile.name);
+      fs.unlinkSync(tmpFile.name);
 
-      await exec.exec(installLinux(osVersionStr, 'x64'));
-      // await exec.exec('bash scripts/install.sh ' + osVersionStr + ' x64')
-      await exec.exec('oscript --version')
+      await exec.exec('oscript --version');
     
     } else {
       throw new Error('OS not support');
@@ -52,6 +56,7 @@ function updatePath() {
 
 function installLinux(version, bitness) {
  var value = [];
+ value.push('#!/bin/bash');
  value.push('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF');
  value.push('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list');
  value.push('sudo apt-get update');
@@ -59,7 +64,7 @@ function installLinux(version, bitness) {
  value.push('curl -v https://oscript.io/downloads/' + version + '/deb?bitness=' + bitness + ' --output os.deb');
  value.push('sudo dpkg -i os.deb');
  value.push('sudo apt install -f');
- return value.join(' && ');
+ return value.join('\n');
 }
 
 run()
