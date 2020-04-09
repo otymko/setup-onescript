056623095a25ba5fcb6fc03ced598ec7640b5d60
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 14:53:56 +0700

    +1

--- index.js
+++ index.js
@@ -28,8 +28,8 @@ async function run() {
       await exec.exec('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF')
       await exec.exec('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list')
       await exec.exec('sudo apt-get update')
-      // await exec.exec('sudo apt-get install mono-complete mono-devel')
-      await exec.exec('- wget -O os.deb http://oscript.io/downloads/' + osVersionStr + '/exe?bitness=x64')
+      await exec.exec('sudo apt-get install mono-complete mono-devel')
+      await exec.exec('curl -v https://oscript.io/downloads/' + osVersionStr + '/exe?bitness=x64 --output os.deb')
       await exec.exec('sudo dpkg -i os.deb; sudo apt install -f')
       await exec.exec('oscript --version')
     
