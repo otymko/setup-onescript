da46f2e0a913b7488b3dd1b3abecb2fd730679d0
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 14:47:44 +0700

    примерка для linux

--- dist/index.js
+++ dist/index.js
@@ -969,6 +969,16 @@ async function run() {
       console.log('Удаление временного файла');
       await fs.unlinkSync('./oscript.exe');
 
+    } else if (patform == 'linux') {
+
+      await exec.exec('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF')
+      await exec.exec('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list')
+      await exec.exec('sudo apt-get update')
+      await exec.exec('sudo apt-get install mono-complete mono-devel')
+      await exec.exec('- wget -O os.deb http://oscript.io/downloads/' + osVersionStr + '/exe?bitness=x64')
+      await exec.exec('sudo dpkg -i os.deb; sudo apt install -f')
+      await exec.exec('oscript --version')
+    
     } else {
       throw new Error('OS not support');
     }
