8919668baf156c32ee4b0bd83005a7f792b8e57f
Oleg Tymko <olegtymko@yandex.ru>
2020-04-09 19:29:00 +0700

    Feature/support linux (#4)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const fs = require('fs');
+const tmp = require('tmp');
 
 const patform = process.platform;
 
@@ -9,6 +10,7 @@ async function run() {
     const osVersion = core.getInput('version');
     var osVersionStr = getVersionString(osVersion);
     console.log('Версия: ' + osVersionStr);
+    console.log('Платформа: ' + patform)
     if (patform == 'win32') {
       console.log('Загрузка');
       await exec.exec('curl -v https://oscript.io/downloads/' + osVersionStr + '/exe?bitness=x64 --output oscript.exe');
@@ -21,12 +23,16 @@ async function run() {
       updatePath();
 
       console.log('Удаление временного файла');
-      await fs.unlinkSync('./oscript.exe');
+      fs.unlinkSync('./oscript.exe');
 
     } else if (patform == 'linux') {
 
-      await exec.exec('bash scripts/install.sh ' + osVersionStr + ' x64')
-      await exec.exec('oscript --version')
+      var tmpFile = tmp.fileSync();
+      fs.writeFileSync(tmpFile.name, installLinux(osVersionStr, 'x64'));
+      await exec.exec('bash ' + tmpFile.name);
+      fs.unlinkSync(tmpFile.name);
+
+      await exec.exec('oscript --version');
     
     } else {
       throw new Error('OS not support');
@@ -46,6 +52,19 @@ function updatePath() {
   const OLD_PATH = process.env.PATH;
   PATH = OLD_PATH + ";C:\/Program Files\/OneScript\/bin;";
   core.exportVariable('Path', PATH);
+}
+
+function installLinux(version, bitness) {
+ var value = [];
+ value.push('#!/bin/bash');
+ value.push('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF');
+ value.push('echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-official.list');
+ value.push('sudo apt-get update');
+ value.push('sudo apt-get install mono-complete mono-devel');
+ value.push('curl -v https://oscript.io/downloads/' + version + '/deb?bitness=' + bitness + ' --output os.deb');
+ value.push('sudo dpkg -i os.deb');
+ value.push('sudo apt install -f');
+ return value.join('\n');
 }
 
 run()
