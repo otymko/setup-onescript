135d481b1f904ef346f1b1258b1587d18677ff29
Tymko Oleg <olegtymko@yandex.ru>
2020-04-09 09:12:19 +0700

    Пробная версия для WIN

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const patform = process.platform;
 
 async function run() {
   try {
     const osVersion = core.getInput('version');
-    console.log('Hello world ${osVersion}');
+    var osVersionStr = getVersionString(osVersion);
+    console.log('Версия: ' + osVersionStr);
+    if (patform == 'win32') {
+      console.log('Загрузка');
+      await exec.exec('curl -v https://oscript.io/downloads/1_3_0/exe?bitness=x86 --output oscript.exe');
+      console.log('Установка');
+      await exec.exec('./oscript.exe /verysilent /norestart');
+      console.log('Удаление временного файла');
+      await exec.exec('del ./oscript.exe');
+    } else {
+      throw new Error('OS not support');
+    }
   }
   catch (error) {
     core.setFailed(error.message);
   }
+}
+
+function getVersionString(value) {
+  var version = osVersion.replace('.', '_');  
+  return version;
 }
 
 run()
