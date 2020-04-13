696480da49a6efdd79d825c19a43ac72f8bcf44a
Tymko Oleg <olegtymko@yandex.ru>
2020-04-13 15:17:19 +0700

    +00

--- dist/index.js
+++ dist/index.js
@@ -2031,6 +2031,7 @@ async function run() {
       fs.unlinkSync('./ovm.exe');
 
       await exec.exec('which oscript');
+      
       await exec.exec('oscript --version');
 
       // await exec.exec('opm install opm');
