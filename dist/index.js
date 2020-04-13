552b89dc9865a7a29700e72ae7dcb73f9e25e9d0
Tymko Oleg <olegtymko@yandex.ru>
2020-04-13 20:02:48 +0700

    проверка кеша!

--- dist/index.js
+++ dist/index.js
@@ -2024,17 +2024,12 @@ async function run() {
         };
       await exec.exec(prefix + './ovm.exe',['which', 'current'], options);
       let pathOscript = getOscriptPath(output);
-
+     
       updateEnvPath(pathOscript);
-
       console.debug('Удаление временного файла');
       fs.unlinkSync('./ovm.exe');
 
-      // await exec.exec('which oscript');
-
       await exec.exec('oscript --version');
-
-      // await exec.exec('opm install opm');
 
   }
   catch (error) {
